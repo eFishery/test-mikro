d60224436fd58d803b8592fb4de325adaa1ed030
ans4175 <ans4175@gmail.com>
2016-09-17 22:51:54 +0700

    init setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='test-mikri',
+      version='0.1',
+      description='Python libs for test-mikro eFishery',
+      url='https://github.com/eFishery/test-mikro',
+      author='ans4175',
+      author_email='ans4175@efishery.com',
+      license='MIT',
+      packages=['test-mikro'],
+      zip_safe=False)
