273694f65c3a679d50069465e5472fcb724d25dc
ans4175 <ans4175@gmail.com>
2016-09-18 09:17:39 +0700

    constants

--- sserial.py
+++ sserial.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python
 
 import time
-import sys
-import glob
 import serial
 import json
 
@@ -20,10 +18,15 @@ def print_decorator(print_type, msg):
     }.get(print_type, '\033[0m')
     print print_color + msg + '\033[0m'
 
+DELAY_MESSAGE = 2
+CRLF = '\r\n'
+BAUDRATE = 115200
+DEFAULT_PORT = '/dev/ttyUSB0'
+
 # configure the serial connections (the parameters differs on the device you are connecting to)
 ser = serial.Serial(
-	port='/dev/ttyUSB0',
-	baudrate=115200,
+	port=DEFAULT_PORT,
+	baudrate=BAUDRATE,
 	parity=serial.PARITY_ODD,
 	stopbits=serial.STOPBITS_TWO,
 	bytesize=serial.SEVENBITS
@@ -35,30 +38,30 @@ if not ser.isOpen():
 fixture_passed = True
 with open('./fixture.json') as data_file:    
     data = json.load(data_file)
-    print_decorator('HEADER',data['title']+'\r\n')
-    test_command = 'AT'.encode() + '\r\n'
+    print_decorator('HEADER',data['title']+CRLF)
+    test_command = 'AT'.encode() + CRLF
     print_decorator('UNDERLINE','initialization....')
     # warming up serial port
     ser.write(test_command)
-    time.sleep(2)
+    time.sleep(DELAY_MESSAGE)
     for fixture in data['fixtures']:
         ser.flushInput()
         command_test = fixture['command'].encode('utf-8')
         assert_test = fixture['assert']
         expect_test = fixture['expect']
         result_test = True
-        command_byte = command_test + '\r\n'
+        command_byte = command_test + CRLF
         print_decorator('BOLD',"-----------------------")
         print_decorator('WARNING',"command: "+command_test)
         ser.write(command_byte)
         incoming_msg = ''
-        time.sleep(2)
+        time.sleep(DELAY_MESSAGE)
         while ser.inWaiting() > 0:
           incoming_msg += ser.read(1)
         if incoming_msg != '':
           # init variable
           incoming_msg = incoming_msg.rstrip()
-          print_decorator('OKBLUE',"reply:\r\n"+incoming_msg)
+          print_decorator('OKBLUE',"reply:"+CRLF+incoming_msg)
           # assertion
           if assert_test == 'exact':
             result_test = incoming_msg == expect_test 
@@ -70,11 +73,14 @@ with open('./fixture.json') as data_file:
           else:
             result_test = False
           # test passed?
+          if not result_test:
+            print_decorator('OKBLUE'," ?expected: "+expect_test)
+            print_decorator('FAIL'," !actual: "+incoming_msg)
           test_color = 'OKGREEN' if result_test else 'FAIL'
           print_decorator(test_color,"test passed: "+str(result_test))
           # fixture passed?
           fixture_passed = fixture_passed and result_test
-          print_decorator('BOLD',"-----------------------\r\n")
+          print_decorator('BOLD',"-----------------------"+CRLF)
     ser.close()
     test_color = 'OKGREEN' if fixture_passed else 'FAIL'
     print_decorator(test_color,"fixtures passed: "+str(fixture_passed))
