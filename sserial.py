3f439b040db545311a9bdbbe69481e074cb33f06
ans4175 <ans4175@gmail.com>
2016-09-18 09:06:31 +0700

    template fixtures and color print

--- sserial.py
+++ sserial.py
@@ -6,6 +6,20 @@ import glob
 import serial
 import json
 
+# print with format
+def print_decorator(print_type, msg):
+    print_color = {
+        'HEADER': '\033[95m',
+        'OKBLUE' : '\033[94m',
+        'OKGREEN' : '\033[92m',
+        'WARNING' : '\033[93m',
+        'FAIL' : '\033[91m',
+        'ENDC' : '\033[0m',
+        'BOLD' : '\033[1m',
+        'UNDERLINE' : '\033[4m',
+    }.get(print_type, '\033[0m')
+    print print_color + msg + '\033[0m'
+
 # configure the serial connections (the parameters differs on the device you are connecting to)
 ser = serial.Serial(
 	port='/dev/ttyUSB0',
@@ -21,9 +35,9 @@ if not ser.isOpen():
 fixture_passed = True
 with open('./fixture.json') as data_file:    
     data = json.load(data_file)
-    print data['title']+'\r\n'
+    print_decorator('HEADER',data['title']+'\r\n')
     test_command = 'AT'.encode() + '\r\n'
-    print 'initialization....'
+    print_decorator('UNDERLINE','initialization....')
     # warming up serial port
     ser.write(test_command)
     time.sleep(2)
@@ -32,9 +46,10 @@ with open('./fixture.json') as data_file:
         command_test = fixture['command'].encode('utf-8')
         assert_test = fixture['assert']
         expect_test = fixture['expect']
+        result_test = True
         command_byte = command_test + '\r\n'
-        print "-----------------------"
-        print "command: "+command_test
+        print_decorator('BOLD',"-----------------------")
+        print_decorator('WARNING',"command: "+command_test)
         ser.write(command_byte)
         incoming_msg = ''
         time.sleep(2)
@@ -43,18 +58,24 @@ with open('./fixture.json') as data_file:
         if incoming_msg != '':
           # init variable
           incoming_msg = incoming_msg.rstrip()
-          print "reply:"
-          print incoming_msg
+          print_decorator('OKBLUE',"reply:\r\n"+incoming_msg)
           # assertion
           if assert_test == 'exact':
             result_test = incoming_msg == expect_test 
           elif assert_test == 'contain':
             result_test = expect_test in incoming_msg
+          elif assert_test == 'contains':
+            for contain in expect_test:
+                result_test = result_test and (contain in incoming_msg)
           else:
             result_test = False
-          print "test passed: "+str(result_test)
+          # test passed?
+          test_color = 'OKGREEN' if result_test else 'FAIL'
+          print_decorator(test_color,"test passed: "+str(result_test))
+          # fixture passed?
           fixture_passed = fixture_passed and result_test
-          print "-----------------------\r\n"
+          print_decorator('BOLD',"-----------------------\r\n")
     ser.close()
-    print "fixtures passed: "+str(result_test)
+    test_color = 'OKGREEN' if fixture_passed else 'FAIL'
+    print_decorator(test_color,"fixtures passed: "+str(fixture_passed))
     exit()
